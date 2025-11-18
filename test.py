c8115662ad5e27c2424f0d85de186c19e4cc1bf0
David Lilien <dlilien@iu.edu>
2025-11-18 13:48:54 -0500

    First commit

---
+++ test.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+# vim:fenc=utf-8
+#
+# Copyright © 2025 dlilien <dlilien@IU-FL419X7XRV>
+#
+# Distributed under terms of the MIT license.
+
+"""
+
+"""
+
+print("We are testing only")
